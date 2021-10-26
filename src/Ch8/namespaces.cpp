4b3ade19f556a95bbaf39e92d358638c1bd7ad5f
adamdenes <adam.denes@icloud.com>
2021-10-26 21:49:09 +0200

    namespaces are fun

---
+++ src/Ch8/namespaces.cpp
@@ -0,0 +1,47 @@
+#include "../std_lib_facilities.h"
+
+namespace X
+{
+    int var;
+    void print()
+    {
+        cout << "X::print() ->\t" << var << endl;
+    }
+}
+
+namespace Y
+{
+    int var;
+    void print()
+    {
+        cout << "Y::print() ->\t" << var << endl;
+    }
+}
+
+namespace Z
+{
+    int var;
+    void print()
+    {
+        cout << "Z::print() ->\t" << var << endl;
+    }
+}
+
+int main()
+{
+    X::var = 7;
+    X::print(); // print X's var
+    using namespace Y;
+    var = 9;
+    print(); // print Y's var
+    {
+        using Z::print;
+        using Z::var;
+        var = 11;
+        print(); // print Z's var
+    }
+    print();    // print Y's var
+    X::print(); // print X's var
+
+    return 0;
+}
