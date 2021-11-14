e46641b0180567b703ec14ff1668354448dd598c
adamdenes <adam.denes@icloud.com>
2021-11-14 09:48:01 +0100

    array drill

---
+++ src/Ch18/array.cpp
@@ -0,0 +1,59 @@
+#include "../std_lib_facilities.h"
+
+// #1.
+int ga[10]{1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+
+// #3.
+void f(int arr[], int size)
+{
+    int la[10];
+
+    for (int i = 0; i < 10; ++i)
+    {
+        la[i] = ga[i];
+    }
+
+    cout << "local array:\t";
+    for (int i = 0; i < 10; ++i)
+    {
+        cout << la[i] << ' ';
+    }
+    cout << endl;
+
+    int *p = new int[size];
+
+    for (int i = 0; i < size; ++i)
+    {
+        p[i] = arr[i];
+    }
+
+    cout << "frestore array:\t";
+    for (int i = 0; i < size; ++i)
+    {
+        cout << p[i] << ' ';
+    }
+    cout << endl;
+
+    delete[] p;
+}
+
+int main()
+{
+    f(ga, 10);
+
+    int aa[10]{
+        1,
+        2 * 1,
+        3 * 2 * 1,
+        4 * 3 * 2 * 1,
+        5 * 4 * 3 * 2 * 1,
+        6 * 5 * 4 * 3 * 2 * 1,
+        7 * 6 * 5 * 4 * 3 * 2 * 1,
+        8 * 7 * 6 * 5 * 4 * 3 * 2 * 1,
+        9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1,
+        10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1};
+
+    f(aa, 10);
+
+    return 0;
+}
